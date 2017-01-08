9a7934729705978cdc727545ab9598e3b391ff04
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-08 01:18:35 +0000

    set up Main component

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Main extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          <Link to='/'>Reduxtagram</Link>
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Main
--- client/reduxtagram.js
+++ client/reduxtagram.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import css from './styles/style.styl'
 
-render(<p>Hello :D</p>, document.getElementById('root'))
+render(<p>Hiya :D</p>, document.getElementById('root'))
