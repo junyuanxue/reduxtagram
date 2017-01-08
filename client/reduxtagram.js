4b2a1b862f644433c1a4ceb08033e3510ed0e4c6
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-08 01:20:10 +0000

    rendered Main

--- client/reduxtagram.js
+++ client/reduxtagram.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import css from './styles/style.styl'
+import Main from './components/Main'
 
-render(<p>Hiya :D</p>, document.getElementById('root'))
+render(<Main />, document.getElementById('root'))
