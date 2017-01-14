7f13aeabcd04524191c8512155663307f463cb74
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-14 21:05:27 +0000

    set up App to connect Main with redux

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,14 +3,14 @@ import { Router, Route, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
 import store, { history } from '../store'
 
-import Main from '../components/Main'
+import App from '../components/App'
 import SinglePhoto from '../components/SinglePhoto'
 import PhotoGrid from '../components/PhotoGrid'
 
 const routes = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path='/' component={Main}>
+      <Route path='/' component={App}>
         <IndexRoute component={PhotoGrid} />
         <Route path='/view/:postId' component={SinglePhoto} />
       </Route>
