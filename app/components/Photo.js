4b9ea3d8a84aefa5cd50c6e5f5072e9d84fc243a
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-14 21:31:55 +0000

    rendering photo content

--- app/components/Photo.js
+++ app/components/Photo.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class Photo extends React.Component {
   render () {
+    const { post, index, comments } = this.props // creates 3 vars post, index, comments from this.props
     return (
       <figure className='grid-figure'>
-        I am a photo
+        <div className='grid-photo-wrap'>
+          <Link to={`view/${post.code}`}>
+            {post.caption}
+          </Link>
+        </div>
       </figure>
     )
   }
