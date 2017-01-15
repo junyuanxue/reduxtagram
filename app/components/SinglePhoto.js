730c79dfcb6eeb1c0a3bba04a00879308b21fc44
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-15 17:16:23 +0000

    Rendering photo on single photo page

--- app/components/SinglePhoto.js
+++ app/components/SinglePhoto.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Photo from './Photo'
 // import Comments
 
 class SinglePhoto extends React.Component {
   render () {
     const code = this.props.params.postId
-    const index = this.props.posts.findIndex((post) => post.code === code)
-    console.log(index)
+    const posts = this.props.posts
+    const index = posts.findIndex((post) => post.code === code)
+    const post = posts[index]
+
     return (
       <div className='single-photo'>
-        SINGLE PHOTO
+        <Photo
+          index={index}
+          post={post}
+          {...this.props} />
       </div>
     )
   }
+}
+
+SinglePhoto.propTypes = {
+  params: PropTypes.object.isRequired,
+  posts: PropTypes.array.isRequired
 }
 
 export default SinglePhoto
