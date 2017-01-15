ad4aef2d897e43152acc2be70df96e39ebc00382
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-15 16:51:33 +0000

    Binding increment likes action to posts reducer

--- app/reducers/posts.js
+++ app/reducers/posts.js
@@ -1,8 +1,17 @@
+// The reducer does the editing of state.
+
 function posts (state = [], action) {
-  console.log('posts action')
-  console.log('posts state: ', state)
-  console.log('posts action: ', action)
-  return state
+  switch (action.type) {        // Every time an action is dispatched, every reducer is going to run,
+    case 'INCREMENT_LIKES':     // hence the switch statement
+      const i = action.index
+      return [                                    // return updated state
+        ...state.slice(0, i),                     // before the one we're updating
+        {...state[i], likes: state[i].likes + 1}, // the post we're updating
+        ...state.slice(i, i + 1)                  // after the one we're updating
+      ]
+    default:
+      return state
+  }
 }
 
 export default posts
@@ -12,10 +21,6 @@ export default posts
 // 1. the action (info what happened)
 // 2. copy of current state
 // pass in action, store --> reducer checking --> return updated store
-
-// The reducer does the editing of state.
-// Every time an action is dispatched, every reducer is going to run,
-// hence the switch statement
 
 // In redux, we use functional programming:
 // we do not mutate our state, we use pure functions.
