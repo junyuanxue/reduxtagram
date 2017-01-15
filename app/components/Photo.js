b7d0653d67d2bc1d8e331e854409be6d56994430
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-15 16:14:17 +0000

    rendering number of comments

--- app/components/Photo.js
+++ app/components/Photo.js
@@ -24,6 +24,12 @@ class Photo extends React.Component {
           <p>{post.caption}</p>
           <div className='control-buttons'>
             <button className='likes'>&hearts; {post.likes}</button>
+            <Link className='button' to={`view/${post.code}`}>
+              <span className='comment-count'>
+                <span className='speech-bubble' />
+                {comments[post.code] ? ` ${comments[post.code].length}` : ` 0`}
+              </span>
+            </Link>
           </div>
         </figcaption>
       </figure>
