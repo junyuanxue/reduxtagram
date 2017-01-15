7bef7e75f2f8963543abc226bfb18076d5aa802e
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-15 18:09:45 +0000

    Using reducer to add comment using reducer composition

--- app/reducers/comments.js
+++ app/reducers/comments.js
@@ -1,7 +1,30 @@
+function postComments (state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT': // return the new state with the new comment
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ]
+    case 'REMOVE_COMMENT':
+      return state
+    default:
+      return state
+  }
+}
+
+// Reducer composition: we only want a **slice** of the state to manage.
+// So rather than changing the entire comments state, we only want to change the state of comments that belong to one post.
+
 function comments (state = [], action) {
-  console.log('comments action')
-  console.log('comments state: ', state)
-  console.log('comments action: ', action)
+  if (action.postId) {
+    return {
+      ...state,                                                   // take current state
+      [action.postId]: postComments(state[action.postId], action) // overwrite this post with a new one
+    }
+  }
   return state
 }
 
