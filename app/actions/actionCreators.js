3943b17fc30f0d16d1235a29e5c8a411f09dfd4d
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-18 21:41:45 +0000

    Adding addPhoto action in actionCreator

--- app/actions/actionCreators.js
+++ app/actions/actionCreators.js
@@ -1,6 +1,14 @@
 // an action: returns an object of what happened and
 // what needs to be changed
 
+export function addPhoto (author, photo) {
+  return {
+    type: 'ADD_PHOTO',
+    author,
+    photo
+  }
+}
+
 export function incrementLikes (index) {
   return {
     type: 'INCREMENT_LIKES',
