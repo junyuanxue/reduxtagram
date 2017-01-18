f44c61e5454ab1bbffaf6bf8084cf9187bfed2d8
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-18 22:39:01 +0000

    Set up saga middleware in store.js

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
+import createSagaMiddleware from 'redux-saga'
 
 // import the root reducer
 import rootReducer from './reducers/index'
+import saga from './services/sagas'
 
 // import posts from './data/posts'
 // import comments from './data/comments'
@@ -19,8 +21,13 @@ const enhancer = compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )
 
+// set up saga
+const sagaMiddleware = createSagaMiddleware()
+
 // const store = createStore(rootReducer, defaultState, enhancer)
-const store = createStore(rootReducer, enhancer)
+const store = createStore(rootReducer, enhancer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(saga)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
