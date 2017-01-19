14a50834a5a6146b26e009c7b153d88c24bd367a
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-19 22:24:32 +0000

    Combining photosActions and commentsActions in actionCreators

--- app/actions/actionCreators.js
+++ app/actions/actionCreators.js
@@ -1,62 +1,11 @@
 // an action: returns an object of what happened and
 // what needs to be changed
+import * as photosActions from './photosActions'
+import * as commentsActions from './commentsActions'
 
-export function fetchPhotos () {
-  return {
-    type: 'FETCH_PHOTOS'
-  }
+const actions = {
+  ...photosActions,
+  ...commentsActions
 }
 
-function handlePhotoFetchSuccess (photos) {
-  return {
-    type: 'PHOTOS_FETCH_SUCCEEDED',
-    photos
-  }
-}
-
-function handlePhotoFetchFailure (message) {
-  return {
-    type: 'PHOTOS_FETCH_FAILED',
-    message
-  }
-}
-
-export function addPhoto (author, photo) {
-  return {
-    type: 'ADD_PHOTO',
-    author,
-    photo
-  }
-}
-
-export function incrementLikes (index) {
-  return {
-    type: 'INCREMENT_LIKES',
-    index
-  }
-}
-
-// Comments
-export function fetchComment (postId) {
-  return {
-    type: 'FETCH_COMMENT',
-    postId
-  }
-}
-
-export function addComment (postId, author, comment) {
-  return {
-    type: 'ADD_COMMENT',
-    postId,
-    author,
-    comment
-  }
-}
-
-export function removeComment (postId, index) {
-  return {
-    type: 'REMOVE_COMMENT',
-    postId,
-    index
-  }
-}
+export default actions
