80ce7258810903b68ab37fdd76e0ff7eacea811f
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-19 22:52:25 +0000

    Added api.fetchComments function

--- app/services/api.js
+++ app/services/api.js
@@ -1,13 +1,22 @@
 import $ from 'jquery'
 
 const token = '249716683.1677ed0.5dd09759c52040c4850bc41d84295983'
-const endpoint = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}`
+const base = 'https://api.instagram.com/v1'
+const endpoint = `${base}/users/self/media/recent/?access_token=${token}`
 
 const api = {
   fetchPhotos: () => {
     return $.ajax({
       type: 'GET',
       url: endpoint,
+      dataType: 'jsonp'
+    }).then(response => response.data)
+  },
+  fetchComments: (postId) => {
+    const url = `${base}/media/${postId}/comments?access_token=${postId}`
+    return $.ajax({
+      type: 'GET',
+      url: url,
       dataType: 'jsonp'
     }).then(response => response.data)
   }
