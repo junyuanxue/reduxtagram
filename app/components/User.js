122134e395bcdbfaf57a51acf16984b05481bcef
Junyuan Xue <junyuanxue.is@gmail.com>
2017-01-22 17:48:26 +0000

    Styling user details

--- app/components/User.js
+++ app/components/User.js
@@ -9,13 +9,15 @@ class User extends React.Component {
     const user = this.props.user
     return (
       <div className='user'>
-        <img src={user.profile_picture} alt={user.username} />
-        <div>
+        <div className='user-image'>
+          <img src={user.profile_picture} alt={user.username} />
+        </div>
+        <div className='user-details'>
           <h3>{user.username}</h3>
           <ul>
-            <li>{user.counts.media} posts</li>
-            <li>{user.counts.followed_by} followers</li>
-            <li>{user.counts.follows} following</li>
+            <li>{user.counts.media} <span>posts</span></li>
+            <li>{user.counts.followed_by} <span>followers</span></li>
+            <li>{user.counts.follows} <span>following</span></li>
           </ul>
           <h4>{user.full_name}</h4>
         </div>
